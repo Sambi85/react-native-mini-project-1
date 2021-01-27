4412f5859e5a41adede76d3d5016aaf42bd0ca56
github username <github email address>
2021-01-27 18:59:32 -0500

    clean-p

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet}
+import {View, Text, StyleSheet} from 'react-native'
 
 const ListScreen = () => {
     return <Text>List Screen</Text>;
-}
+};
 
 const styles = StyleSheet.create({
 
