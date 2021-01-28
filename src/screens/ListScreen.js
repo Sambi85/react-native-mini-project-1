ae34ac160a59cab78e68c52de672aad93bf07ea2
github username <github email address>
2021-01-27 19:28:24 -0500

    list-screen-update

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import { FlatList, View, Text, StyleSheet} from 'react-native'
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+
+    const friends = [
+        
+        { name: "Friend #1" },
+        { name: "Friend #2" },
+        { name: "Friend #3" },
+        { name: "Friend #4" },
+        { name: "Friend #5" },
+        { name: "Friend #6" },
+        { name: "Friend #7" },
+        { name: "Friend #8" },
+        { name: "Friend #9" },
+        { name: "Friend #10" }
+    ]
+    return (
+
+    <FlatList 
+        data={friends} 
+        renderItem={({ item }) => {
+            return <Text>{item.name}</Text>
+        }}
+    />
+
+    );
 };
 
 const styles = StyleSheet.create({
