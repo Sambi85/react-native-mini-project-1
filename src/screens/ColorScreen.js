a7773ec044294c0896a8f6dd138e8d9c52638a4d
github username <github email address>
2021-01-28 19:38:27 -0500

    color-screen-update

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -9,7 +9,7 @@ const ColorScreen = () => {
         <View>
             <Button title="Add a Color"/>
             <View 
-            style={{ height: 100, width: 100, backgroundColor: 'rgb(0, 25, 0)' 
+            style={{ height: 100, width: 100, backgroundColor: 'rgb(0, 255, 0)' 
         }}/>
         </View>
     );
@@ -20,7 +20,7 @@ const randomRGB = () => {
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
 
-    return `rgb(${red}, ${blue}, ${green})`
+    return `rgb(${red}, ${blue}, ${green})`;
 }
 
 const styles = StyleSheet.create({});
