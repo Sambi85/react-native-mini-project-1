3ea4288b57fdf7dba7a0c7bc85a467e3eb3e441a
github username <github email address>
2021-01-29 19:13:58 -0500

    square-screen-button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import ComponentsScreen from './src/screens/ComponentsScreen';
-import ListScreen from './src/screens/ListScreen';
-import ImageScreen from './src/screens/ImageScreen';
-import CounterScreen from './src/screens/CounterScreen'
+
 import ColorScreen from './src/screens/ColorScreen'
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import CounterScreen from './src/screens/CounterScreen'
+import HomeScreen from './src/screens/HomeScreen';
+import ImageScreen from './src/screens/ImageScreen';
+import ListScreen from './src/screens/ListScreen';
+import SquareScreen from './src/screens/SquareScreen';
 
 const navigator = createStackNavigator(
   {
@@ -14,7 +16,8 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
-    Color: ColorScreen
+    Color: ColorScreen,
+    Square: SquareScreen
   },
 
   {
