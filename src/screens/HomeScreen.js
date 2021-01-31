1c8b47d2f8fcdf0cc31a787511a53e21738d5aac
github username <github email address>
2021-01-30 19:59:12 -0500

    text-screen-setup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -36,6 +36,11 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Square Demo"
       />
 
+      <Button
+        onPress={()=> navigation.navigate("Text")}
+        title="Go to Text Demo"
+      />
+
     </View>
   ) 
 };
